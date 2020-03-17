78f3d2a626455d1fd88ec62a77a272c0f1b6ea95
Ozhan Yilmaz <ozhanyilmaz@gmail.com>
2020-03-17 21:16:30 +0100

    done

--- server.js
+++ server.js
@@ -1,23 +1,29 @@
-'use strict';
+"use strict";
 
-var express = require('express');
-var cors = require('cors');
+var express = require("express");
+var cors = require("cors");
+const multer = require("multer");
+var upload = multer({ dest: "uploads/" });
 
 // require and use "multer"...
 
 var app = express();
-
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-     res.sendFile(process.cwd() + '/views/index.html');
-  });
-
-app.get('/hello', function(req, res){
-  res.json({greetings: "Hello, API"});
+app.get("/", function(req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log('Node.js listening ...');
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+  console.log("req.file ", req.file);
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
+});
+
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Node.js listening ...");
 });
